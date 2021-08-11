1978aa0d02f68c44147f913ca8c1eca5d2808a5d
zhaojianhua <1161223922@qq.com>
2021-08-11 22:42:41 +0800

    【新增】：路由权限判断

---
+++ src/config/constans.js
@@ -0,0 +1,9 @@
+const Constans = {
+    accessList: {
+        _super: 0, //超级管理员
+        _admin: 1, //系统
+        _ordinary: 198 //普通
+    }
+}
+
+export default Constans;
---
+++ src/config/utils.js
@@ -0,0 +1,45 @@
+/**
+ * @method 权限判断
+ * @param {*} name  跳转路由
+ * @param {*} access 权限，
+ * @param {*} routerArray 路由列表
+ */
+function isAccess(name, access, routerArray) {
+    var routerPermissionJudge = (list) => {
+        return list.some(item => {
+            if (item.children && item.children.length) {
+                return routerPermissionJudge(item.children);
+            } else if (name === item.name) {
+                return hasAccess(access, item);
+            }
+        });
+    }
+    return routerPermissionJudge(routerArray);
+}
+
+/**
+ * @method 是否拥有权限
+ * @param {*} access 
+ * @param {*} item 
+ * @returns 
+ */
+function hasAccess(access, item) {
+    if (item.meta && item.meta.access) {
+        return isCludesAccess(access, item.meta.access);
+    }
+    return true;
+}
+
+/**
+ * @method 判断权限数组中是否存在当前权限
+ * @param {*} access 
+ * @param {*} routeAccess 
+ * @returns 
+ */
+function isCludesAccess(access, routeAccess) {
+    return routeAccess.includes(access);
+}
+
+export default {
+    isAccess
+}
