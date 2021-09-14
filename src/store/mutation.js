947512d4eaa72ab376b773211b0d2073337478a6
zhaojianhua <1161223922@qq.com>
2021-09-14 19:54:58 +0800

    格式化

--- src/store/mutation.js
+++ src/store/mutation.js
@@ -1,14 +1,14 @@
 import utils from '../utils/utils'
 console.log(utils);
 const mutations = {
-  SET_TOKEN:(state, token)=>{
+  SET_TOKEN: (state, token) => {
     debugger
     state.token = token;
-    utils.setSessionItem('token',token);
+    utils.setSessionItem('token', token);
   },
-  SET_USERINFO:(state, userInfo)=>{
+  SET_USERINFO: (state, userInfo) => {
     state.userInfo = userInfo;
-    utils.setSessionItem('userInfo',JSON.stringify(userInfo));
+    utils.setSessionItem('userInfo', JSON.stringify(userInfo));
   }
 }
 export default mutations;
