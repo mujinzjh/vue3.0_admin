3c4180c798f7aef717cc4078ae5fba7708ddaa21
zhaojianhua <1161223922@qq.com>
2021-09-30 14:19:44 +0800

    【修改】：国际化文件

--- src/locale/index.js
+++ src/locale/index.js
@@ -1,12 +1,12 @@
 /*
  * @Author: mujin
  * @Date: 2021-09-29 11:09:29
- * @LastEditTime: 2021-09-29 11:20:44
+ * @LastEditTime: 2021-09-30 14:17:53
  * @Description:引入国际化配置文件
  */
 
 import Vue from "vue";
-import iView from "ivew";
+import iView from "iview";
 import VueI18n from 'vue-i18n';
 import en from 'iview/dist/locale/en-US';
 import zh from 'iview/dist/locale/zh-CN';
